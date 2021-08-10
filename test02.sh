324ce02aea61bc31c8a3ff2d980b33d092f87e2d
Arzaan Mirza <78077697+arzaanmirza@users.noreply.github.com>
2021-08-10 23:04:47 +1000

    Delete test02.sh

--- test02.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/dash
-#The perl program recognises the string interpolation when using ' ' when shell doesn't.
-# Perl program isn't suppose to recognise it. It should print andrew rather than $name.
-name=andrew
-echo 'Hello my $name is Tim'
